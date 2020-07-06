de299aa9e86ac4b7d349d59af52db9c289b71a70
Jere Alho <34038726+jalho@users.noreply.github.com>
2020-07-06 16:36:35 +0300

    Include build

---
+++ client/build/precache-manifest.ec19061e1393fe6e098c0dece8fffe85.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecb5502932899d03118959dcca5e52a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ffbddfc2fde53d8e7971",
+    "url": "/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "82d9868af987f2421ccb",
+    "url": "/static/js/2.3131da8e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3131da8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffbddfc2fde53d8e7971",
+    "url": "/static/js/main.7663fb3f.chunk.js"
+  },
+  {
+    "revision": "89c81471bbbd626c98d4",
+    "url": "/static/js/runtime-main.6ec7e816.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
