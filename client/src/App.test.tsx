dfa61e3e2d1046c471ff2e729b71dea91223f8e8
Jere Alho <34038726+jalho@users.noreply.github.com>
2020-07-07 18:34:10 +0300

    Fix template test

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders work in progress text", () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const WIPViewText = getByText(/kesken/i);
+  expect(WIPViewText).toBeInTheDocument();
 });
