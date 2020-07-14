2583b9723f4fa7adc258fc8264437986be790ed8
jalho <34038726+jalho@users.noreply.github.com>
2020-07-14 19:32:15 +0300

    Document and refactor React.lazy usage in App

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,18 @@
 import { lazy } from "react";
 
+/* "React.lazy takes a function that must call a dynamic import().
+This must return a Promise which resolves to a module with a
+default export containing a React component." */
 const App = lazy(() => {
+  // bundle async operations; import home page & arbitrary minimum delay
   return Promise.all([
-    import("./home/index"),
-    new Promise(resolve => setTimeout(resolve, 2500))
+    import("./home/index"), // dynamically import home page, resolves with `Module`
+    new Promise((resolve) => setTimeout(resolve, 2500)) // minimum delay time, resolves with `undefined`
   ])
-  .then(([moduleExports]) => moduleExports);
+  // when the bundle is ready, return the imported module
+  .then((resolvedResults) => {
+    return resolvedResults[0]; // the dynamically imported module, i.e. home page
+  });
 });
 
 
