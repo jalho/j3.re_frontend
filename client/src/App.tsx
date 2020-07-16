b9000a7e52afaca6812d922fee31bbee4e299fc1
jalho <34038726+jalho@users.noreply.github.com>
2020-07-16 19:45:09 +0300

    Simplify React.lazy comments in App

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,19 +3,23 @@ import { lazy } from "react";
 /* "React.lazy takes a function that must call a dynamic import().
 This must return a Promise which resolves to a module with a
 default export containing a React component." */
-const App = lazy(() => {
-  // bundle async operations; import home page & arbitrary minimum delay
-  return Promise.all([
-    // dynamically import home page
-    import("./views/home"), // resolves with `Module`
-    // minimum delay time
-    new Promise((resolve) => setTimeout(resolve, 2500)) // resolves with `undefined`
-  ])
-  // when the bundle is ready, return the imported module
-  .then((resolvedResults) => {
-    return resolvedResults[0]; // the dynamically imported module, i.e. home page
-  });
-});
+const App = lazy(
+  
+  () => {
 
+    // bundle promises; import home page & arbitrary minimum delay
+    return Promise.all([
+      // two promises; one for dynamic import and another for arbitrary delay
+      import("./views/home"), // --> React component
+      new Promise((resolve) => setTimeout(resolve, 2500)) // --> `undefined`
+    ])
+
+    // when the bundle is ready, return the imported React component
+    .then((resolvedResults) => {
+      return resolvedResults[0];
+    });
+  }
+
+);
 
 export default App;
