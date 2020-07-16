0dab9bc0f80c41cccccfb3574ad464d2d47cedfd
jalho <34038726+jalho@users.noreply.github.com>
2020-07-16 19:51:27 +0300

    Move min delay to a variable

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,10 @@
 import { lazy } from "react";
 
+/**
+ * The minimum time in milliseconds until 'Landing' view is exited.
+ */
+const minimumDelay = 2500;
+
 /* "React.lazy takes a function that must call a dynamic import().
 This must return a Promise which resolves to a module with a
 default export containing a React component." */
@@ -10,7 +15,7 @@ const App = lazy(
     const resolvedResults = await Promise.all([
       // two promises; one for dynamic import and another for arbitrary delay
       import("./views/home"),
-      new Promise((resolve) => setTimeout(resolve, 2500)) // --> `undefined`
+      new Promise((resolve) => setTimeout(resolve, minimumDelay)) // --> `undefined`
     ]);
     return resolvedResults[0];
   }
