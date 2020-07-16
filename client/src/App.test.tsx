4aff22a4b092b5f80390862be1d65cc54aa7cf90
jalho <34038726+jalho@users.noreply.github.com>
2020-07-16 12:53:18 +0300

    Remove template tests

--- client/src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import App from "./App";
-
-test("renders work in progress text", () => {
-  const { getByText } = render(<App />);
-  const WIPViewText = getByText(/kesken/i);
-  expect(WIPViewText).toBeInTheDocument();
-});
