bd796afb51480ac906071c5358148992841a1e2b
jalho <34038726+jalho@users.noreply.github.com>
2020-07-20 16:06:40 +0300

    Refactor DOM elements' styling

--- client/src/views/landing/index.tsx
+++ client/src/views/landing/index.tsx
@@ -6,8 +6,8 @@ import Face from "../../components/Face";
 const Landing: React.FC = () => {
   // TODO: Center the landing view somehow to the middle of the viewport!
   return (
-    <div className="fullView">
-      <div className="landing">
+    <div className="landingContainer">
+      <div className="landingContent">
         <Header text="j3.re" />
         <div id="landingFace"><Face /></div>
       </div>
