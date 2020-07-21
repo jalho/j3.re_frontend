44f712f6bf33ece4a511d211ab45d1920543af28
jalho <34038726+jalho@users.noreply.github.com>
2020-07-21 14:00:41 +0300

    Add CV content and refactor

--- client/src/resources/translations.ts
+++ client/src/resources/translations.ts
@@ -4,13 +4,17 @@ export const translations = {
     "translation": {
       "current": "current",
       "CV": "CV",
+      "Education": "Education",
       "English": "English",
       "Finnish": "suomi",
       "Here's going to be some sort of CV.": "Here's going to be some sort of CV.",
       "Here will be summaries of things I have created.": "Here will be summaries of things I have created.",
       "Here will be the main view.": "Here will be the main view.",
       "Language": "Language",
+      "major in computer Science": "major in computer science",
+      "minor in mathematics": "minor in mathematics",
       "Portfolio": "Portfolio",
+      "start year 2019": "start year 2019",
       "Work in progress.": "Work in progress."
     }
   },
@@ -19,13 +23,17 @@ export const translations = {
     "translation": {
       "current": "nykyinen",
       "CV": "Ansioluettelo",
+      "Education": "Koulutus",
       "English": "English",
       "Finnish": "suomi",
       "Here's going to be some sort of CV.": "Tähän tulee jonkinlainen ansioluettelo.",
       "Here will be summaries of things I have created.": "Yhteenvedot tekemistäni jutuista tulevat tähän.",
       "Here will be the main view.": "Tähän tulee päänäkymä.",
       "Language": "Kieli",
+      "major in computer science": "pääaineena tietojenkäsittelytiede",
+      "minor in mathematics": "sivuaineena matematiikka",
       "Portfolio": "Työt",
+      "start year 2019": "aloitusvuosi 2019",
       "Work in progress.": "Työn alla."
     }
   }
--- client/src/views/cv/index.tsx
+++ client/src/views/cv/index.tsx
@@ -1,14 +1,27 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
 
+import UTU from "../../components/UTU";
+import Header from "../../components/Header";
+
 const CV: React.FC = () => {
 const { t } = useTranslation();
 
   return (
     <>
-      <p>
-        {t("Here's going to be some sort of CV.")}
-      </p>
+      <Header text={t("Education")} />
+
+      <div className="CV_container">
+        <div id="CV_UTU">
+          <UTU />
+        </div>
+
+        <ul>
+          <li>{t("major in computer science")}</li>
+          <li>{t("minor in mathematics")}</li>
+          <li>{t("start year 2019")}</li>
+        </ul>
+      </div>
     </>
   );
 };
