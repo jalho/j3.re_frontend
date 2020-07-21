02eb73ecbcf5a38e90cfd24f0d111c8c7672c42b
jalho <34038726+jalho@users.noreply.github.com>
2020-07-21 11:43:02 +0300

    Modularize translation resources

--- client/src/i18n.ts
+++ client/src/i18n.ts
@@ -1,38 +1,11 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-
-// TODO: Move translations to `.json`!
-const resources = {
-  en: {
-    translation: {
-      "Work in progress.": "Work in progress.",
-      "CV": "CV",
-      "Portfolio": "Portfolio",
-      "Sandbox": "Sandbox",
-      "Language": "Language",
-      "Here's going to be some sort of CV.": "Here's going to be some sort of CV.",
-      "Here will be summaries of things I have created.": "Here will be summaries of things I have created.",
-      "Here will be the main view.": "Here will be the main view."
-    }
-  },
-  fi: {
-    translation: {
-      "Work in progress.": "Työn alla.",
-      "CV": "Ansioluettelo",
-      "Portfolio": "Työt",
-      "Sandbox": "Hiekkalaatikko",
-      "Language": "Kieli",
-      "Here's going to be some sort of CV.": "Tähän tulee jonkinlainen ansioluettelo.",
-      "Here will be summaries of things I have created.": "Yhteenvedot tekemistäni jutuista tulevat tähän.",
-      "Here will be the main view.": "Tähän tulee päänäkymä."
-    }
-  }
-};
+import { translations } from "./resources/translations";
 
 i18n
   .use(initReactI18next) // pass i18n to react-i18next
   .init({
-    resources, // translations
+    resources: translations,
     lng: "en", // default language
     keySeparator: false, // e. g. "messages.welcome"
     interpolation: {
---
+++ client/src/resources/translations.ts
@@ -0,0 +1,42 @@
+export const translations = {
+
+  "en": {
+    "translation": {
+      "Work in progress.": "Work in progress.",
+      "CV": "CV",
+      "Portfolio": "Portfolio",
+      "Sandbox": "Sandbox",
+      "Language": "Language",
+      "Here's going to be some sort of CV.": "Here's going to be some sort of CV.",
+      "Here will be summaries of things I have created.": "Here will be summaries of things I have created.",
+      "Here will be the main view.": "Here will be the main view."
+    }
+  },
+
+  "fi": {
+    "translation": {
+      "Work in progress.": "Työn alla.",
+      "CV": "Ansioluettelo",
+      "Portfolio": "Työt",
+      "Sandbox": "Hiekkalaatikko",
+      "Language": "Kieli",
+      "Here's going to be some sort of CV.": "Tähän tulee jonkinlainen ansioluettelo.",
+      "Here will be summaries of things I have created.": "Yhteenvedot tekemistäni jutuista tulevat tähän.",
+      "Here will be the main view.": "Tähän tulee päänäkymä."
+    }
+  },
+
+  "es": {
+    "translation": {
+      "Work in progress.": "Los Pollos Hermanos.",
+      "CV": ":D",
+      "Portfolio": "😎",
+      "Sandbox": "Asd",
+      "Language": ":-DD",
+      "Here's going to be some sort of CV.": "Los Pollos Hermanos.",
+      "Here will be summaries of things I have created.": "Los Pollos Hermanos jajajajaja!",
+      "Here will be the main view.": "Sombrero."
+    }
+  }
+
+};
