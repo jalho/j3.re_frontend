3b0c3e0ff2cd7d4a0e392eb6353e04d111756c6d
jalho <34038726+jalho@users.noreply.github.com>
2020-07-24 13:23:30 +0300

    Initialize GraphQL implementation

--- client/src/views/leave_note/index.tsx
+++ client/src/views/leave_note/index.tsx
@@ -1,12 +1,36 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
+import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
+import Button from "react-bootstrap/Button";
+
+const testQuery = () => {
+  const client = new ApolloClient({
+    uri: "https://j3re-backend.herokuapp.com/", // TODO: Get from environment!
+    cache: new InMemoryCache()
+  });
+
+  client
+  .query({
+    query: gql`
+      query GetNotes {
+        notes {
+          id
+          content
+          time
+        }
+      }
+    `
+  })
+  .then(result => console.log(result));
+}
 
 const LeaveNote: React.FC = () => {
   const { t } = useTranslation();
 
   return (
     <>
-      {t("Here's going to be an option to leave a public note.")}
+      <p>{t("Work in progress.")}</p>
+      <Button variant="primary" onClick={() => testQuery()}>Test GraphQL in console</Button>
     </>
   );
 };
