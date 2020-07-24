8c70ea76f54076320faa997e4234478eb13f392e
jalho <34038726+jalho@users.noreply.github.com>
2020-07-24 14:47:34 +0300

    Render info & notes only if there are some notes

--- client/src/views/leave_note/index.tsx
+++ client/src/views/leave_note/index.tsx
@@ -15,22 +15,25 @@ const NOTES = gql`
 
 const LeaveNote: React.FC = () => {
   const { t } = useTranslation();
-  const { loading, data } = useQuery(NOTES);
+  const { /* loading, error, */ data } = useQuery(NOTES);
+
+  // create JSX elements of fetched notes
+  let noteElements: Array<JSX.Element> = [];
+  if (data) noteElements = data.notes.map((note: { id: any; time: any; content: any; }) => ( // TODO: Use `Note` type!
+    <p key={note.id} style={{ backgroundColor: "rgb(1, 2, 3, 0.1)", borderRadius: "0.5em", padding: "1em" }}>
+      <b>{`${new Date(note.time).getUTCDate()}.${new Date(note.time).getUTCMonth()+1}.${new Date(note.time).getUTCFullYear()}`}</b>
+      <br/>
+      <i>{note.content}</i>
+    </p>
+  ));
 
   return (
     <>
       <div style={{ textAlign: "center", maxWidth: "30em" }}>
         <p>{t("Here's going to be a notes leaving feature. The notes won't be translated, instead they will remain in their original language.")}</p>
-        <p>{t("Below are some example fetched from a database.")}</p>
       </div>
-      {loading && <p>Loading...</p>}
-      {data && data.notes.map((note: { id: any; time: any; content: any; }) => ( // TODO: Use `Note` type!
-        <p key={note.id} style={{ backgroundColor: "rgb(1, 2, 3, 0.1)", borderRadius: "0.5em", padding: "1em" }}>
-          <b>{`${new Date(note.time).getUTCDate()}.${new Date(note.time).getUTCMonth()+1}.${new Date(note.time).getUTCFullYear()}`}</b>
-          <br/>
-          <i>{note.content}</i>
-        </p>
-      ))}
+      {/* render info text & notes only if there are some notes */}
+      {noteElements.length > 0 && [<p style={{ textAlign: "center", maxWidth: "30em" }}>{t("Below are some example fetched from a database.")}</p>, ...noteElements]}
     </>
   );
 };
