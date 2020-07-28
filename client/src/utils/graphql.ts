5da2c081d66e7e70a675117d65b954577a191883
jalho <34038726+jalho@users.noreply.github.com>
2020-07-28 15:42:28 +0300

    Implement password check

--- client/src/utils/graphql.ts
+++ client/src/utils/graphql.ts
@@ -1,11 +1,21 @@
 import { gql } from '@apollo/client';
 
 export const GET_ALL_NOTES = gql`
-query Notes {
-  notes {
-    id
-    content
-    time
+  query Notes {
+    notes {
+      id
+      content
+      time
+    }
   }
-}
+`;
+
+export const GET_ALL_USERS = gql`
+  query Users {
+    users {
+      id
+      username
+      passwordHash
+    }
+  }
 `;
--- client/src/views/login/index.tsx
+++ client/src/views/login/index.tsx
@@ -1,19 +1,44 @@
 import React, { FormEvent, useState } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import bcrypt from "bcryptjs";
+import { useQuery } from '@apollo/client';
+import { GET_ALL_USERS } from "../../utils/graphql";
 
 const Login: React.FC = () => {
   const [username, setUsername] = useState<string>("");
   const [password, setPassword] = useState<string>("");
+  const { error, data } = useQuery(GET_ALL_USERS);
 
-  const loginHandler = (e: FormEvent) => {
-    e.preventDefault(); // prevent reloading
-    
-    // TODO: Do the actual login!
-
-    // clear fields
+  /**
+   * Clear the login form's input fields.
+   */
+  const clearFields = (): void => {
     setUsername("");
     setPassword("");
+  }
+
+  /**
+   * Find the correct username's details from previously fetched users list, and if found,
+   * compare the entered password to the username's associated password hash.
+   * @param e event emitted from login form
+   */
+  const loginHandler = (e: FormEvent) => {
+    e.preventDefault(); // prevent reloading
+    const userLoggingIn = data.users.find((user: { username: string; }) => user.username === username);
+    if (!userLoggingIn) {
+      console.log("No such user!"); // TODO: Add notification to UI.
+      clearFields();
+      return;
+    }
+    bcrypt.compare(password, userLoggingIn.passwordHash, (_error, result) => {
+      if (result) {
+        console.log("Correct password!"); // TODO: Add notification to UI. Generate a token and use that in requests.
+      } else {
+        console.log("Incorrect password!"); // TODO: Add notification to UI.
+      }
+    });
+    clearFields();
   };
 
   return (
@@ -29,9 +54,13 @@ const Login: React.FC = () => {
           <Form.Control type="password" placeholder="Password" onChange={e => setPassword(e.target.value)} value={password} />
         </Form.Group>
 
-        <Button variant="primary" type="submit">
+        <Button variant="primary" type="submit" disabled={!data}>
           Log in
         </Button>
+        {error && (
+          <Form.Text>
+            Server is not operational.
+          </Form.Text>)}
       </Form>
     </>
   );
