66d48d3214b045cdcc6de129a4453f38d974a68b
jalho <34038726+jalho@users.noreply.github.com>
2020-07-28 15:42:54 +0300

    Use local development server when in dev mode

--- client/src/utils/helpers.ts
+++ client/src/utils/helpers.ts
@@ -2,6 +2,8 @@
  * Get backend's URI to be used in Apollo Client or elsewhere.
  */
 export const getBackendURI = (): string => {
+  if (process.env.NODE_ENV === "development") return "http://localhost:4000/";
+
   const uri = process.env.REACT_APP_BACKEND_URI;
   if (!uri) throw new Error("Environment variable REACT_APP_BACKEND_URI missing!");
   return uri;
