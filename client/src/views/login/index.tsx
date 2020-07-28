bc209e59013fb6e1c8415db1c5a33c9b8d29f5aa
jalho <34038726+jalho@users.noreply.github.com>
2020-07-28 14:29:41 +0300

    Control login form fields

--- client/src/views/login/index.tsx
+++ client/src/views/login/index.tsx
@@ -3,11 +3,17 @@ import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 
 const Login: React.FC = () => {
-  const [username, setUsername] = useState<string>();
+  const [username, setUsername] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
 
   const loginHandler = (e: FormEvent) => {
-    e.preventDefault();
-    // TODO!
+    e.preventDefault(); // prevent reloading
+    
+    // TODO: Do the actual login!
+
+    // clear fields
+    setUsername("");
+    setPassword("");
   };
 
   return (
@@ -15,12 +21,12 @@ const Login: React.FC = () => {
       <Form onSubmit={loginHandler}>
         <Form.Group>
           <Form.Label>Username</Form.Label>
-          <Form.Control type="text" placeholder="For example email" onChange={e => setUsername(e.target.value)} />
+          <Form.Control type="text" placeholder="For example email" onChange={e => setUsername(e.target.value)} value={username} />
         </Form.Group>
 
         <Form.Group controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control type="password" placeholder="Password" onChange={e => setPassword(e.target.value)} value={password} />
         </Form.Group>
 
         <Button variant="primary" type="submit">
