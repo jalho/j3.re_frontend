a1eddd24682e515f1599e81029d0eb54a205d6bf
jalho <34038726+jalho@users.noreply.github.com>
2020-07-28 12:31:22 +0300

    Initialize submit handler

--- client/src/views/login/index.tsx
+++ client/src/views/login/index.tsx
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { FormEvent, useState } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 
 const Login: React.FC = () => {
+  const [username, setUsername] = useState<string>();
+
+  const loginHandler = (e: FormEvent) => {
+    e.preventDefault();
+    // TODO!
+  };
+
   return (
     <>
-      <Form>
-        <Form.Group controlId="formBasicEmail">
+      <Form onSubmit={loginHandler}>
+        <Form.Group>
           <Form.Label>Username</Form.Label>
-          <Form.Control type="email" placeholder="For example email" />
+          <Form.Control type="text" placeholder="For example email" onChange={e => setUsername(e.target.value)} />
         </Form.Group>
 
         <Form.Group controlId="formBasicPassword">
