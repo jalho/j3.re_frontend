f5c3fab3726ffa8df0d92b0e82e3655c726e199a
jalho <34038726+jalho@users.noreply.github.com>
2020-07-28 18:58:56 +0300

    Add hashing helper function

--- client/src/utils/helpers.ts
+++ client/src/utils/helpers.ts
@@ -1,3 +1,5 @@
+import bcrypt from "bcryptjs";
+
 /**
  * Get backend's URI to be used in Apollo Client or elsewhere.
  */
@@ -7,4 +9,13 @@ export const getBackendURI = (): string => {
   const uri = process.env.REACT_APP_BACKEND_URI;
   if (!uri) throw new Error("Environment variable REACT_APP_BACKEND_URI missing!");
   return uri;
+};
+
+/**
+ * Hash a given password using https://www.npmjs.com/package/bcryptjs with 10 "salt".
+ * @param password to hash
+ * @returns password hash
+ */
+export const hashPassword = (password: string): string => {
+  return bcrypt.hashSync(password, 10);
 };
