b9cb5582f64f429928cbb6c388a27998e29d6a08
jalho <34038726+jalho@users.noreply.github.com>
2020-07-29 12:56:28 +0300

    Remove remaining references to bcryptjs
    The dependency was removed, but some references I forgot about.

--- client/src/utils/helpers.ts
+++ client/src/utils/helpers.ts
@@ -1,5 +1,3 @@
-import bcrypt from "bcryptjs";
-
 /**
  * Get backend's URI to be used in Apollo Client or elsewhere.
  */
@@ -9,13 +7,4 @@ export const getBackendURI = (): string => {
   const uri = process.env.REACT_APP_BACKEND_URI;
   if (!uri) throw new Error("Environment variable REACT_APP_BACKEND_URI missing!");
   return uri;
-};
-
-/**
- * Hash a given password using https://www.npmjs.com/package/bcryptjs with 10 "salt".
- * @param password to hash
- * @returns password hash
- */
-export const hashPassword = (password: string): string => {
-  return bcrypt.hashSync(password, 10);
 };
