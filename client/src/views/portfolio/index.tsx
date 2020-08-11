7453c68f6f61c6fe2cc0c0dc1fa3fd46b555c796
jalho <34038726+jalho@users.noreply.github.com>
2020-08-11 19:12:40 +0300

    Draft Portfolio view

--- client/src/views/portfolio/index.tsx
+++ client/src/views/portfolio/index.tsx
@@ -1,15 +1,38 @@
 import React from "react";
-import { useTranslation } from "react-i18next";
+// import { useTranslation } from "react-i18next";
+
+import Card from "../../components/Card";
 
 const Portfiolio: React.FC = () => {
-  const { t } = useTranslation();
+  // const { t } = useTranslation();
+
+  const dummyData = [
+    {
+      title: "Title One",
+      infoText: "Info text one.",
+      items: ["1111111111", "aaaaaaaaaaa", "AAAAAAAAAA"]
+    },
+    {
+      title: "Title Two",
+      infoText: "Info text two.",
+      items: ["22222222222", "bbbbbbbb", "BBBBBBBBBBBBBBBBBB"]
+    },
+    {
+      title: "Title Three",
+      infoText: "Info text three.",
+      items: ["CCCCCCCCCCC", "33333333333", "cccccccccccccccccccc"]
+    },
+    {
+      title: "Title Four",
+      infoText: "Info text four.",
+      items: ["DDDDDDDDDDDDDDDD", "dddddddddddddddd", "444444444"]
+    },
+  ];
 
   return (
-    <>
-      <p>
-        {t("Here will be summaries of things I have created.")}
-      </p>
-    </>
+    <div id="portfolio">
+      {dummyData.map((data, idx) => (<Card key={idx} infoText={data.infoText} items={data.items}/>))}
+    </div>
   );
 };
 
